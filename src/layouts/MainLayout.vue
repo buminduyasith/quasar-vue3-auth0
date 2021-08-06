8658ec8a19bc5dbc5136fb971d7d1907da41ce2c
Bumindu Yasith <bumiduyasith@gmail.com>
2021-08-06 23:21:05 +0530

    add welome text signout button mainlayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -6,7 +6,17 @@
 
         <q-toolbar-title> Quasar App </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <span>Hi </span>
+
+        <q-btn-dropdown stretch flat icon="account_circle" class="q-ml-xs">
+          <q-list>
+            <q-item clickable v-close-popup>
+              <q-item-section>
+                <q-item-label>Signout</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown>
       </q-toolbar>
     </q-header>
 
